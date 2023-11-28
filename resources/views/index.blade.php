bd5f84d5e216e53a1f3df09573a5ed1b2da1ca51
nailahazhr <nailahazhr@gmail.com>
2023-11-28 12:59:36 +0700

    pertemuan 28 Nop

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,14 +6,14 @@
 
     <h3>Data Pegawai</h3>
 
-    <a href="/pegawai/tambah" class="btn btn-primary"> + Tambah Pegawai Baru</a>
+    <a href="/pegawai/tambah" class="btn btn-primary mt-3"> + Tambah Pegawai Baru</a>
 
     <br>
     <p class="mt-4">Cari Data Pegawai berdasarkan Nama:</p>
-	<form action="/pegawai/cari" method="GET">
-		<input class="form-control mr-2 w-25" type="text" name="cari" placeholder="Cari Pegawai .." value="{{ old("cari", isset($cari) ? $cari : '') }}">
-		<input class="btn btn-primary my-2 my-sm-0" type="submit" value="CARI">
-	</form>
+	<form action="/pegawai/cari" method="GET" class="d-flex">
+        <input class="form-control mr-2" type="text" name="cari" placeholder="Cari Pegawai .." value="{{ old("cari", isset($cari) ? $cari : '') }}">
+        <input class="btn btn-primary my-2 my-sm-0" type="submit" value="CARI">
+    </form>
     <br/>
 
     <table class="table table-striped table-hover">
@@ -31,8 +31,8 @@
                 <td>{{ $p->pegawai_umur }}</td>
                 <td>{{ $p->pegawai_alamat }}</td>
                 <td>
+                    <a href="/pegawai/edit/{{ $p->pegawai_id }}" class="btn btn-success">View</a>
                     <a href="/pegawai/edit/{{ $p->pegawai_id }}" class="btn btn-warning">Edit</a>
-                    |
                     <a href="/pegawai/hapus/{{ $p->pegawai_id }}" class="btn btn-danger">Hapus</a>
                 </td>
             </tr>
--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -1,28 +1,53 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
-</head>
-<body>
+@extends('master')
 
-	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
+@section('title', 'Data Pegawai')
+
+@section('konten')
+
 	<h3>Data Pegawai</h3>
 
-	<a href="/pegawai"> Kembali</a>
-
-	<br/>
 	<br/>
 
-	<form action="/pegawai/store" method="post">
+	<form action="/pegawai/store" method="post" class="form-horizontal">
 		{{ csrf_field() }}
-		Nama   : <input type="text" name="nama"> <br/>
+
+        <div class="form-group row">
+            <label for="Name" class="col-sm-1 col-form-label">Nama:</label>
+            <div class="col-sm-6">
+                <input type="text" class="form-control" id="Nama" name="Nama">
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="Jabatan" class="col-sm-1 col-form-label">Jabatan:</label>
+            <div class="col-sm-6">
+                <input type="text" class="form-control" id="Jabatan" name="Jabatan">
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="Umur" class="col-sm-1 col-form-label">Umur:</label>
+            <div class="col-sm-6">
+                <input type="text" class="form-control" id="Umur" name="Umur">
+            </div>
+        </div>
+
+        <div class="form-group row">
+            <label for="Alamat" class="col-sm-1 col-form-label">Alamat:</label>
+            <div class="col-sm-6">
+                <input type="text" class="form-control" id="Alamat" name="Alamat">
+            </div>
+        </div>
+        <br>
+
+        <a class="btn btn-success" href="/pegawai"> Kembali</a>
+        <input class="btn btn-primary" type="submit" value="Simpan Data">
+
+		{{-- Nama   : <input type="text" name="nama"> <br/>
 		Jabatan: <input type="text" name="jabatan"> <br/>
 		Umur   : <input type="number" name="umur"> <br/>
 		Alamat : <textarea name="alamat"></textarea> <br/>
-		<input type="submit" value="Simpan Data">
+		<input type="submit" value="Simpan Data"> --}}
 	</form>
 
-
-
-</body>
-</html>
+@endsection
