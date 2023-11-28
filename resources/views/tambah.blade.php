94dfe48beb23bed7e8d011495bdedbd7ef55976b
nailahazhr <nailahazhr@gmail.com>
2023-11-28 12:22:31 +0700

    Pertemuan 28-November-2023

--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -15,10 +15,10 @@
 
 	<form action="/pegawai/store" method="post">
 		{{ csrf_field() }}
-		Nama <input type="text" name="nama"> <br/>
-		Jabatan <input type="text" name="jabatan"> <br/>
-		Umur <input type="number" name="umur"> <br/>
-		Alamat <textarea name="alamat"></textarea> <br/>
+		Nama   : <input type="text" name="nama"> <br/>
+		Jabatan: <input type="text" name="jabatan"> <br/>
+		Umur   : <input type="number" name="umur"> <br/>
+		Alamat : <textarea name="alamat"></textarea> <br/>
 		<input type="submit" value="Simpan Data">
 	</form>
 
